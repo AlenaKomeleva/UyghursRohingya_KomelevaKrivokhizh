04ad46bcb4c3487acf58efb55c8a9d3ca1d16c83
Alena Komeleva <54914030+AlenaKomeleva@users.noreply.github.com>
2025-02-11 13:33:36 +0100

    Create Okaz.py

---
+++ Okaz.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+articles = []
+for url in urls:
+	response = requests.get(url)
+	soup = BeautifulSoup(response.content, 'html.parser')
+	html_article = soup.find('div', class_='bodyText')
+	article_text = html_article.get_text()
+	articles.append(article_text)
